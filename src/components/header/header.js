043f9acaa2c8bed75b55af1ffff8ec64add8e7f8
Isabela Maria <isabela10014@hotmail.com>
2021-03-23 17:11:04 -0300

    Menu ok

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,26 +1,38 @@
 import React from 'react'
 import './style.css'
 import logo from '../../assets/images/logo.svg'
+
 const Header = () => {
-  return (
+	const Features = null
+	const Support = null
+	const Blog = null
+
+	const [abrir, setAbrir] = React.useState("false")
+	function Abrir() {
+		setAbrir(!abrir);
+	}
+	
+	return (
     <header>
 		<div className="container">
-			<div className="flex-menu-mobile">
-				<div className="logo">
-					<img src={logo} alt="Logo" className="img-response"/>
-					<span>tinyone</span>
+			<div className="header">
+				<div className="flex-menu-mobile">
+					<div className="logo">
+						<img src={logo} alt="Logo" className="img-response"/>
+						<span>tinyone</span>
+					</div>
+					<div className="menu-burger above-overlay" onClick={Abrir}>
+						<div className={abrir ? "bar" : "bar animate"}></div>
+					</div>
 				</div>
-				<div className="menu-burger above-overlay">
-					<div className="bar"></div>
-				</div>
+				<nav className={abrir ? "menu" : "menu active"}>
+					<ul>
+						<li><a href={Features} className="active">Features</a></li>
+						<li><a href={Support}>Support</a></li>
+						<li><a href={Blog}>Blog</a></li>
+					</ul>
+				</nav>
 			</div>
-			<nav className="menu">
-				<ul>
-					<li><a className="active">Features</a></li>
-					<li><a>Support</a></li>
-					<li><a>Blog</a></li>
-				</ul>
-			</nav>
 		</div>
 	</header>
   );
